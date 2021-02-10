aef034188445532dec3e4b9837c7ec30ce606840
Ernie Massei <Emassei@users.noreply.github.com>
2021-02-10 15:40:02 -0500

    add some tests

--- polls/tests.py
+++ polls/tests.py
@@ -7,6 +7,15 @@ from .models import Question
 
 
 class QuestionModelTests(TestCase):
+    def test_was_published_with_really_old_date(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=6)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
     def test_was_published_recently_with_old_question(self):
         """
         was_published_recently() returns False for questions whose pub_date
@@ -24,3 +33,23 @@ class QuestionModelTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
+
+    def test_was_published_recently_in_the_middle_of_the_day(self):
+        """
+        was_published_recently() returns True for questions whose pub_date
+        is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=12, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
+    def test_was_published_in_the_future(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is one day in the future
+        """
+        time = timezone.now() + datetime.timedelta(days=1)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), False)
+
+
